4210e23779e01d8183359420fc6a371592556804
AliHassen <01014194042a@gmail.com>
2024-08-06 16:08:05 +0300

    feat: chapter details logic

--- lib/modules/layout/quran/screen/Chapter_details.dart
+++ lib/modules/layout/quran/screen/Chapter_details.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islamic_app/core/themes/app_themes.dart';
 import 'package:islamic_app/core/widgets/background_widget.dart';
+import 'package:islamic_app/modules/layout/quran/widgets/verse_content.dart';
 
-class ChapterDetailsScreen extends StatelessWidget {
+class ChapterDetailsScreen extends StatefulWidget {
   static const routeName = 'ChapterDetailsScreen';
 
-  const ChapterDetailsScreen({super.key});
+  ChapterDetailsScreen({super.key});
+
+  @override
+  State<ChapterDetailsScreen> createState() => _ChapterDetailsScreenState();
+}
+
+class _ChapterDetailsScreenState extends State<ChapterDetailsScreen> {
+  List<String> verses = [];
 
   @override
   Widget build(BuildContext context) {
     var arg = ModalRoute.of(context)!.settings.arguments as ChapterDetailsArg;
+
+    if (verses.isEmpty) {
+      _readSuraData(arg.index);
+    }
+
     return BackgroundWidget(
         child: Scaffold(
             backgroundColor: Colors.transparent,
@@ -20,14 +35,34 @@ class ChapterDetailsScreen extends StatelessWidget {
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(25)),
               margin: EdgeInsets.symmetric(vertical: 64, horizontal: 24),
-              child: ListView.builder(
-                physics: BouncingScrollPhysics(),
-                itemCount: 100,
-                itemBuilder: (context, index) {
-                  return Text('');
-                },
-              ),
+              child: verses.isEmpty
+                  ? Center(child: CircularProgressIndicator())
+                  : ListView.separated(
+                      physics: BouncingScrollPhysics(),
+                      itemCount: verses.length,
+                      separatorBuilder: (context, index) {
+                        return Container(
+                          width: double.infinity,
+                          height: 2,
+                          color: AppTheme.primalyLightColor,
+                          margin: EdgeInsets.symmetric(horizontal: 64),
+                        );
+                      },
+                      itemBuilder: (context, index) {
+                        return VerseContent(
+                            content: verses[index], index: index);
+                      },
+                    ),
             )));
+  }
+
+  void _readSuraData(int fileIndex) async {
+    String fileContent =
+        await rootBundle.loadString('assets/suras/${fileIndex + 1}.txt');
+    List<String> lines = fileContent.trim().split('\n');
+    setState(() {
+      verses = lines;
+    });
   }
 }
 
---
+++ lib/modules/layout/quran/widgets/verse_content.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class VerseContent extends StatelessWidget {
+  final String content;
+  final int index;
+
+  VerseContent({
+    super.key,
+    required this.content,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Text(
+        textDirection: TextDirection.rtl,
+        '${content} (${index + 1}).',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 25,
+        ),
+      ),
+    );
+  }
+}
